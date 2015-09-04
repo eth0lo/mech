faf3bb8fb31e719548fbfa3253cc99b597cd38c3
Jean Carlos Meninno <jeancarlos.meninno@wuaki.tv>
2015-09-04 15:10:57 +0200

    Change the signature of the middleware

--- src/node/index.js
+++ src/node/index.js
@@ -1,15 +1,13 @@
-var viewContext = require('./view_context');
-var $           = require('./jquery');
 var Backbone    = require('./backbone');
 var middleware  = require('./express/render_middleware');
-var viewEngine  = require('./express/view_engine');
 
 module.exports = {
   Backbone:   Backbone,
-  $:          $,
   express: {
-    middleware: middleware,
-    viewEngine: viewEngine
-  },
-  viewContext: viewContext
+    middleware: {
+      render: {
+        helpers: middleware
+      }
+    }
+  }
 };
