6ef17437c83c7ef6218b2b76d779764d4f1c8eaa
Jean Carlos Meninno <jeancarlos.meninno@wuaki.tv>
2015-09-04 15:06:42 +0200

    Make backbone control the instance of jquery

--- src/node/backbone.js
+++ src/node/backbone.js
@@ -1,12 +1,17 @@
-var viewContext = require('./view_context');
-var $           = require('./jquery');
-var Backbone    = require('backbone');
+var domino    = require('domino');
+var jQuery    = require('jquery');
+var Backbone  = require('backbone');
+var transport = require('./transport');
+var window    = domino.createWindow();
+var $         = jQuery(window);
 
-Backbone.$ = $;
+$.ajaxTransport(transport);
+
+Backbone.$   = $
 
 // Modify Backbone to be able to render server side
 Backbone.View.prototype._createElement = function(tagName) {
-    return viewContext.document.createElement(tagName);
+  return window.document.createElement(tagName);
 }
 
 module.exports = Backbone;
