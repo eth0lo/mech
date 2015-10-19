de08170a70687efaeede8d6cdd0531c56a2edcdc
Jean Carlos Meninno <jeancarlos.meninno@wuaki.tv>
2015-10-19 17:26:14 +0200

    Add test for the raw attribute in the model

---
+++ test/save_response.js
@@ -0,0 +1,26 @@
+var test  = require('tape-catch');
+var sinon = require('sinon');
+var nock  = require('nock');
+
+var mech  = require('../src/node');
+
+test('#raw', function(t) {
+  var ModelOne = mech.Model.extend({
+    name: 'modelOne',
+    url: 'http://sample.com/one'
+  });
+
+  var response =  {model: 'one'};
+  nock('http://sample.com')
+    .get('/one')
+    .reply(200,response);
+
+  var modelOne = new ModelOne();
+  modelOne.fetch({
+    success: function() {
+      t.deepEqual(modelOne.raw, response)
+    }
+  });
+
+  t.end();
+});
